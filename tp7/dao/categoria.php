5a3821d02bd2183358190a16e6e87783686de31e
TOMAS JARABROVISKI <43627670@ort>
2019-07-01 12:01:23 -0300

    dao

--- tp7/dao/categoria.php
+++ tp7/dao/categoria.php
@@ -3,12 +3,48 @@ include_once ($_SERVER["DOCUMENT_ROOT"] . '/model/categoria.php');
 
 class CategoriaDao {
 
-    public static function ObtenerPorID($id) {
-        //devuelve un objeto de tipo persona
-    }// get
+    public static function ObtenerPorID($id) 
+    {
+        $cat = new Categoria();
+        $params = array
+        (
+            ":id" => $id
+        )
+        $DBH = new PDO("mysql:host=127.0.0.1;dbname=sistema", "root", "");
+		$query = 'SELECT * FROM categorias where id = :id';
+		$STH = $DBH->prepare($query);
+        $STH->setFetchMode(PDO::FETCH_ASSOC);
+        $STH->execute($params);
+        if ($STH->rowCount() > 0)
+        {
+            $row = $STH->fetch();
+            $cat->idCategoria = $row['id'];
+            $cat->nombreCategoria = $row['nombre'];
+            
+        }
+    };
 
-    public static function ObtenerTodos() {
-        //devuelve un array de objetos de tipo persona
+    public static function ObtenerTodos() 
+    {
+        $cont = 0;
+        $arrayCat = new array();
+        $DBH = new PDO("mysql:host=127.0.0.1;dbname=sistema", "root", "");
+		$query = 'SELECT * FROM categorias';
+		$STH = $DBH->prepare($query);
+        $STH->setFetchMode(PDO::FETCH_ASSOC);
+        $STH->execute($params);
+        if ($STH->rowCount() > 0)
+        {
+            while($row = $STH->fetch())
+            {
+                $cat = new Categoria();
+                $cat->idCategoria = $row['id'];
+                $cat->nombreCategoria = $row['nombre'];
+                $arrayCat[$cont] = $cat;
+                $cont++;
+            }
+        }
+        
     }
 
     public static function nuevo($item) {
